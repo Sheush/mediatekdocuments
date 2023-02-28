5c8063de8644b99ae11ac6a863f61f52801b3f05
Maxat <Maxat@DESKTOP-RUR87SH>
2023-02-28 11:36:28 +0100

    Ajoutez des fichiers projet.

---
+++ MediaTekDocuments/model/ApiAccess.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+namespace MediaTekDocuments.model
+{   public class ApiAccess
+    {
+        private static ApiAccess instance = null;
+
+        private ApiAccess()
+        {
+            
+        }
+
+        public static ApiAccess Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ApiAccess();
+                }
+                return instance;
+            }
+        }
+    }
+}
